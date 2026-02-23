5acf788e4d8802438799f05e61711a7904e07dbe
Nareshadithyaa <nareshadithyaa961@gmail.com>
2026-02-23 11:09:34 +0530

    feature/UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,13 @@
+
 public class PalindromeCheckerApp {
-  public  static void main(String[] args) {
-      System.out.println("Hello Palindrome");
+
+    public static void main(String[] args) {
+        showWelcomeMessage();
+    }
+
+    public static void showWelcomeMessage() {
+        System.out.println("Welcome to the Palindrome Checker Application");
+        System.out.println("Application Version: 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
