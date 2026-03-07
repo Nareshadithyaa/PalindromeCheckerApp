f2400edc8a9d107ef9c13733fec7a8c9aabbf02a
Nareshadithyaa <nareshadithyaa961@gmail.com>
2026-03-07 19:09:03 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,19 @@
 import java.util.Scanner;
-
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a sentence: ");
         String str = sc.nextLine();
-
-        str = str.replace(" ", "").toLowerCase();
-
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         String reverse = "";
-
         for (int i = str.length() - 1; i >= 0; i--) {
             reverse = reverse + str.charAt(i);
         }
-
         if (str.equals(reverse)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
         }
-
         sc.close();
     }
 }
