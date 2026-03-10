89a5e340171dc4c6e10572634010e07ad16f70f1
Nareshadithyaa <nareshadithyaa961@gmail.com>
2026-03-10 09:57:19 +0530

    feature/UC13

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,6 @@
 import java.util.*;
-interface PalindromeStrategy {
-    boolean checkPalindrome(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
+public class PalindromeCheckerApp {
+    static boolean stackMethod(String input) {
         Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
             stack.push(c);
@@ -15,9 +12,7 @@ class StackStrategy implements PalindromeStrategy {
         }
         return true;
     }
-}
-class DequeStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
+    static boolean dequeMethod(String input) {
         Deque<Character> deque = new ArrayDeque<>();
         for (char c : input.toCharArray()) {
             deque.addLast(c);
@@ -29,27 +24,22 @@ class DequeStrategy implements PalindromeStrategy {
         }
         return true;
     }
-}
-public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word: ");
         String input = sc.nextLine();
-        System.out.println("Choose Strategy: 1.Stack 2.Deque");
-        int choice = sc.nextInt();
-        PalindromeStrategy strategy;
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
-        boolean result = strategy.checkPalindrome(input);
-        if (result) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not a Palindrome");
-        }
+        long start1 = System.nanoTime();
+        boolean r1 = stackMethod(input);
+        long end1 = System.nanoTime();
+        long start2 = System.nanoTime();
+        boolean r2 = dequeMethod(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("Stack Result: " + r1);
+        System.out.println("Stack Time: " + (end1 - start1));
+        System.out.println("Deque Result: " + r2);
+        System.out.println("Deque Time: " + (end2 - start2));
         sc.close();
     }
 }
