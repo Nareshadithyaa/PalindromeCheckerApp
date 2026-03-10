5fbd0f5955c095731f18eb20c45012982ad67638
Nareshadithyaa <nareshadithyaa961@gmail.com>
2026-03-10 09:49:37 +0530

    feature/UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -9,6 +9,7 @@ public class PalindromeCheckerApp {
         }
         return checkPalindrome(str, start + 1, end - 1);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word: ");
