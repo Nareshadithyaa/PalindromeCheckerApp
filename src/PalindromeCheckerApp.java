6ed50efce5174a103ad1130254293d1fb14392e8
Nareshadithyaa <nareshadithyaa961@gmail.com>
2026-03-10 09:09:47 +0530

    features/UC8

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,49 @@
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-        showWelcomeMessage();
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+        }
     }
-
-    public static void showWelcomeMessage() {
-        System.out.println("Welcome to the Palindrome Checker Application");
-        System.out.println("Application Version: 1.0");
-        System.out.println("System initialized successfully.");
+    static Node head = null;
+    static void insert(char c) {
+        Node newNode = new Node(c);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    static boolean isPalindrome() {
+        String original = "";
+        String reversed = "";
+        Node temp = head;
+        while (temp != null) {
+            original += temp.data;
+            reversed = temp.data + reversed;
+            temp = temp.next;
+        }
+        return original.equals(reversed);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
+        for (int i = 0; i < input.length(); i++) {
+            insert(input.charAt(i));
+        }
+        if (isPalindrome()) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
     }
 }
